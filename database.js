3bd32a6a1b77461e8112927cf681577d3c362436
Zack Barbari <zackb@unc.edu>
2022-04-16 17:26:43 -0400

    forgot to edit query endpoint

--- database.js
+++ database.js
@@ -4,9 +4,9 @@ const Database = require('better-sqlite3')
 
 const db = new Database('log.db')
 
-const isInitialized = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
+const query = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
 
-if (isInitialized.get() == undefined) {
+if (query.get() == undefined) {
     const sqlInit = `
         CREATE TABLE accesslog ( 
             id INTEGER PRIMARY KEY, remoteaddr STRING, remoteuser STRING, time STRING, method STRING, url STRING, protocol STRING, httpversion STRING, status INTEGER, referer STRING, useragent STRING);
