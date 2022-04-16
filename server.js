862f59c4a0c3e37c9cde90cec9e63bb852e92888
Zack Barbari <zackb@unc.edu>
2022-04-16 17:31:41 -0400

    error test endpoint added

--- server.js
+++ server.js
@@ -81,6 +81,10 @@ if (debug != "false") {
   })
 }
 
+app.get('/app/error', (req, res) => {
+  throw new Error("Error test")
+})
+
 function coinFlip() {
     if (Math.random() >= 0.5) {
       return "heads"
